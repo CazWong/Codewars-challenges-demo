39ec5cb7ecb0f5d3d86ccfd47b22b221f2d9f3d4
Caz Wong <carolynaichi@gmail.com>
2022-07-12 13:43:51 +1000

    added new codewars challenge

---
+++ 8kyu/lost-without-a-map.rb
@@ -0,0 +1,10 @@
+# DESCRIPTION:
+# Given an array of integers, return a new array with each value doubled.
+
+# My Solution:
+def maps(x)
+  # code here
+  # iterate through each number. - each
+  # multiple the number * 2 - block
+  x.map { |number| number * 2 }
+end
