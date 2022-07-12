120da388eb7ef4cb66bb0562f705d3cc09513687
Caz Wong <carolynaichi@gmail.com>
2022-07-12 13:50:03 +1000

    String-repeat challenge added

---
+++ 8kyu/string-repeat.rb
@@ -0,0 +1,15 @@
+# TODO:
+# Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+# Examples (input -> output)
+# 6, "I"     -> "IIIIII"
+# 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+# My Solution:
+def repeat_str (n, s)
+  s * n
+ end
+
+# Alternative:
+def repeat_str (n, s)
+  "#{s * n}"
