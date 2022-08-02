e2da7f099c38112ee8cebeb217123cfca45dfcae
Caz Wong <carolynaichi@gmail.com>
2022-08-02 20:12:46 +1000

    new Kata solved

---
+++ 8kyu/removing_elements.rb
@@ -0,0 +1,5 @@
+# Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+def remove_every_other(array)
+  array.select.with_index { |_, index| index.even? }
+end
